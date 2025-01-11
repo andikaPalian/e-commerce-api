d3d457fe2ee2dcaba193bf05c520ce1124d6f0f6
Andika Palian <andika12des@gmail.com>
2025-01-11 19:30:41 +0800

    mengupdate user dan product model

--- models/product.model.js
+++ models/product.model.js
@@ -1,5 +1,19 @@
 import mongoose from "mongoose";
 
+const sizeStockSchema = new mongoose.Schema({
+    size: {
+        type: String,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+}, {
+    _id: false,
+});
+
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -25,10 +39,7 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    sizes: {
-        type: Array,
-        required: true,
-    },
+    sizeStock: [sizeStockSchema],
     bestseller: {
         type: Boolean,
     },
@@ -39,6 +50,33 @@ const productSchema = new mongoose.Schema({
 }, {
     timestamps: true,
 });
+
+// Mengecek ketersediaan stock
+productSchema.methods.checkStock = function(size, quantity) {
+    const sizeStock = this.sizeStock.find(item => item.size === size);
+    // Memeriksa apakaha size atau ukuran ada
+    if (!sizeStock) {
+        return false;
+    };
+    // Membandingkan stok dengan quantity
+    // Jika ukuran ditemukan, fungsi memeriksa apakah stok yang tersedia (sizeStock.stock) cukup untuk memenuhi kuantitas yang diminta (quantity).
+    return sizeStock.stock >= quantity;
+};
+
+// Method untuk mengurangi stock
+productSchema.methods.decreaseStock = function(size, quantity) {
+    const sizeStock = this.sizeStock.find(item => item.size === size);
+    // Memeriksa apakaha size atau ukuran ada
+    if (!sizeStock) {
+        return false;
+    };
+    // Memeriksa apakah stok mencukupi
+    if (sizeStock.stock < quantity) {
+        return false;
+    };
+    sizeStock.stock -= quantity;
+    return true;
+}
 
 const productModel = mongoose.model("Product", productSchema);
 
--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,28 @@
 import mongoose from "mongoose";
 
+const cartItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    size: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    }
+}, {
+    _id: false,
+});
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -14,9 +37,12 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    cartData: {
-        type: Object,
-        default: {},
+    cart: {
+        items: [cartItemSchema],
+        totalAmount: {
+            type: Number,
+            default: 0,
+        }
     },
     role: {
         type: String,
@@ -27,6 +53,14 @@ const userSchema = new mongoose.Schema({
     minimize: false,
     timestamps: true,
 });
+
+// Menghitung total harga dari semua barang yang ada di cart
+userSchema.methods.calculateTotalAmount = function() {
+    this.cart.totalAmount = this.cart.items.reduce((total, item) => {
+        return total + (item.price * item.quantity);
+    }, 0);
+    return this.cart.totalAmount;
+};
 
 const userModel = mongoose.model("User", userSchema);
 
