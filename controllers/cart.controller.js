eb0be51736546ceb02e11adb570a4535caf80237
Andika Palian <andika12des@gmail.com>
2025-01-12 10:42:42 +0800

    menyelesaikan cart controllera tapi belum di test

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -61,4 +61,108 @@ const addToCart = async (req, res) => {
     };
 };
 
-export {addToCart};
+const getCart = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const user = await userModel.findById(userId).populate("cart.items.productId", "name image price");
+        res.status(200).json({data: user.cart});
+    } catch (error) {
+        console.error("Error getting cart:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+const updateCartItem = async (req, res) => {
+    try {
+        const {productId, size, quantity} = req.body;
+        const userId = req.user.id;
+
+        const user = await userModel.findById(userId);
+        const existingProduct = user.cart.items.findIndex(item => item.productId.toString() === productId && item.size === size);
+
+        if (existingProduct === -1) {
+            return res.status(404).json({message: "Product not found in cart"});
+        };
+
+        const product = await productModel.findById(productId);
+        if (!product.checkStock(size, quantity)) {
+            return res.status(400).json({message: "Insufficient stock"});
+        };
+        // Kalau jumlah barangnya di update ke 0
+        if (quantity <= 0) {
+            // Hapus item
+            user.cart.splice(existingProduct, 1);
+        } else {
+            // Update quantity
+            user.cart.items[existingProduct].quantity = quantity
+        };
+
+        user.calculateCartTotal();
+        await user.save();
+        await user.populate("cart.items.productId", "name image price");
+        res.status(200).json({
+            message: "Cart updated successfully",
+            data: user.cart,
+        });
+    } catch (error) {
+        console.error("Error updating cart item:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+const removeFromCart = async (req, res) => {
+    try {
+        const {productId, size} = req.body;
+        const userId = req.user.id;
+
+        const user = await userModel.findById(userId);
+        const existingProduct = user.cart.items.findIndex(item => item.productId.toString() === productId && item.size === size);
+        if (existingProduct === -1) {
+            return res.status(404).json({message: "Product not found in cart"});
+        };
+
+        user.cart.items.splice(existingProduct, 1);
+        user.calculateCartTotal();
+        await user.save();
+        res.status(200).json({
+            message: "Product removed from cart successfully",
+            data: user.cart,
+        });
+    } catch (error) {
+        console.error("Error removing product from cart:", error);
+        return res.status(500),json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+const clearCart = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const user = await userModel.findById(userId);
+        user.cart = {
+            items: [],
+            totalAmount: 0,
+        };
+        await user.save();
+        res.status(200).json({
+            message: "Cart cleared successfully",
+            data: user.cart,
+        });
+    } catch (error) {
+        console.error("Error clearing cart:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message || "An unexpected error occurred",
+        });
+    };
+};
+
+export {addToCart, getCart, updateCartItem, removeFromCart, clearCart};
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import userValidation from "../middleware/userValidation.js";
-import { addToCart } from "../controllers/cart.controller.js";
+import { addToCart, clearCart, getCart, removeFromCart, updateCartItem } from "../controllers/cart.controller.js";
 
 const cartRouter = express.Router();
 
+cartRouter.get("/get", userValidation, getCart);
 cartRouter.post("/add", userValidation, addToCart);
+cartRouter.put("/update", userValidation, updateCartItem);
+cartRouter.delete("/remove", userValidation, removeFromCart);
+cartRouter.delete("/clear", userValidation, clearCart);
 
 export default cartRouter;
